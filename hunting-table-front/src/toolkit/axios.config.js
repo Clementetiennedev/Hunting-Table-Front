a4a09270176987ec1915469f3cd41cf8a7e80938
JLWear <thomas@PC-Thomas.localdomain>
2023-10-13 09:56:20 +0200

    axios config

---
+++ hunting-table-front/src/toolkit/axios.config.js
@@ -0,0 +1,27 @@
+const api = (method, path) => {
+    const config = {
+      baseUrl: "https://pokeapi.co/api/v2/",
+      path: path,
+      token: null,
+    };
+  
+    let url = () => {
+      return config.baseUrl + config.path;
+    };
+  
+    let headers = () => {
+      return config.token
+        ? {
+            Authorization: `Bearer ${config.token}`,
+          }
+        : {};
+    };
+  
+    return {
+      method: method,
+      url: url(),
+      headers: headers(),
+    };
+  };
+  
+  export default api;
